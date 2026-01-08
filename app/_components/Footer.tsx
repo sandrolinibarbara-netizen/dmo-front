cdd0b7435eee5b412a8e7c1816c498fc7c7df7a8
barba <sandrolini.barbara@nebbialab.it>
2026-01-08 10:53:42 +0100

    New footer

--- app/_components/Footer.tsx
+++ app/_components/Footer.tsx
@@ -32,10 +32,10 @@ export default function Footer({links, contacts} : {links:any, contacts:any}) {
                 <div className="w-full lg:w-[35vw] text-center lg:text-left">
                     <p className="mb-8">Destination Management Organization di Cremona</p>
                     <p className="mb-4">Contatti</p>
-                    <p>Sede: {contacts.indirizzo}</p>
-                    <p>REA: {contacts.rea} | P.IVA {contacts.pIva}</p>
-                    <p>Cap.Soc. {contacts['capitale_sociale']}</p>
-                    <p>PEC: {contacts.pec}</p>
+                    <p><a href={`mailto:${contacts.email}`}>{contacts.email}</a></p>
+                    <p><a href={`tel:${contacts.telefono.split(' ').join('')}`}>{contacts.telefono}</a></p>
+                    <p className="mt-2">Coordinamento a cura di <a className="underline text-corpo-orange" href="https://www.reindustria.com/" target="_blank">REI Reindustria</a>
+                    </p>
                 </div>
                 <div className="w-full lg:w-[45vw] flex justify-center lg:justify-start gap-6 underline text-corpo-orange">
                     <ul className="flex flex-col gap-6 text-center lg:text-left">
